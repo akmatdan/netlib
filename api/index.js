79d7514f57d45ac184d72e608fe9ba3b53cbb39c
Dan Akmat <akmatdan@gmail.com>
2021-07-19 11:12:19 +0600

    DB connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.listen(8800, () =>) {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("DB Connection Successfull!"))
+.catch((err) => console.log(err))
+app.listen(3001, () => {
   console.log("Backend server is running!")
-}
+})
