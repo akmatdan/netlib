4014e09eaa49f5311819b7cdb1d205f7999796e0
Dan Akmat <akmatdan@gmail.com>
2021-07-23 00:17:27 +0600

    jwt created

---
+++ api/routes/users.js
@@ -0,0 +1,16 @@
+const router  = require('express').Router();
+const User = require('../models/User')
+const CryptoJS = require('cryto-js')
+
+//UPDATE
+router.put("/:id", async (req, res)=> {
+  
+})
+
+//DELETE
+
+//GET
+
+//GET ALL
+
+//GET USER STATUS
